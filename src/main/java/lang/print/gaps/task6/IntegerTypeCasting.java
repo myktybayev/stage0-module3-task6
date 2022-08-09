ba9391ebe01d1ea701bec5ddc4575135ae0c1e5e
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-10 00:14:53 +0600

    task6 perfect

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -1,12 +1,17 @@
 package lang.print.gaps.task6;
 
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IntegerTypeCasting {
     public static void main(String[] args) {
 
         long first = 1000L;
-        int second = 1000;
+        int second = (int)first;
 
         System.out.println(first);
         System.out.println(second);
+
     }
 }
